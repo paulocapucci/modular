ea99d5c4245a2648f7cab0ddfafd81a5096e886b
Paulo Henrique Capuzzo <paulocapucci@gmail.com>
2023-11-15 08:15:14 -0300

    crud completo

--- exemplo/urls.py
+++ exemplo/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from exemplo import views
+from exemplo.views import ClienteCreate
 
 app_name = 'exemplo'
 
 urlpatterns = [
-    path('', views.ClienteList.as_view(), name='list')
+    path('', views.ClienteList.as_view(), name='list'),
+    path('create/', views.ClienteCreate.as_view(), name='create'),
+    path('update/<int:pk>/', views.ClienteUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', views.ClienteDelete.as_view(), name='delete'),
+    path('detail/<int:pk>/', views.ClienteDetail.as_view(), name='detail'),
 ]
--- exemplo/views.py
+++ exemplo/views.py
@@ -1,6 +1,28 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.urls import reverse_lazy
+
 from exemplo.models import Cliente
 
+# -----------------------------
 class ClienteList(ListView):
     model = Cliente
     queryset = Cliente.objects.all()
+# -----------------------------
+class ClienteCreate(CreateView):
+    model = Cliente
+    fields = '__all__'
+    success_url = reverse_lazy('exemplo:list')
+# -----------------------------
+class ClienteUpdate(UpdateView):
+    model = Cliente
+    fields = '__all__'
+    success_url = reverse_lazy('exemplo:list')    
+# -----------------------------
+class ClienteDelete(DeleteView):
+    model = Cliente
+    fields = '__all__'
+    success_url = reverse_lazy('exemplo:list')    
+# -----------------------------
+class ClienteDetail(DetailView):
+    queryset = Cliente.objects.all()
+# -----------------------------
